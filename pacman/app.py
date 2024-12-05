aff8f402a258157cf5724958042fd4664c298834
yuthimreddy <hva4zb@virginia.edu>
2024-12-04 21:13:28 -0500

    fixing lambda fucntion

--- pacman/app.py
+++ pacman/app.py
@@ -4,8 +4,9 @@ import mysql.connector
 import boto3
 from chalice import Chalice
 
-app = Chalice(app_name='backend')
+app = Chalice(app_name='pacman')
 app.debug = True
+
 
 # s3 things
 ## UPDATE NEXT LINE
@@ -14,36 +15,37 @@ s3 = boto3.client('s3')
 
 # base URL for accessing the files
 ## UPDATE NEXT LINE
-baseurl = 'http://hva4zb-dp1-spotify.s3-website-us-east-1.amazonaws.com/'
 
-# database things
+#link in FAQs
+baseurl = 'https://hva4zb-dp1-spotify.s3.us-east-1.amazonaws.com/'
 DBHOST = os.getenv('DBHOST')
 DBUSER = os.getenv('DBUSER')
 DBPASS = os.getenv('DBPASS')
 DB = os.getenv('DB')
-db = mysql.connector.connect(user=DBUSER, host=DBHOST, password=DBPASS, database=DB)
-cur = db.cursor()
+
 
 # file extensions to trigger on
 _SUPPORTED_EXTENSIONS = (
-    '.json'
+  '.json'
 )
 
 # ingestor lambda function
 @app.on_s3_event(bucket=S3_BUCKET, events=['s3:ObjectCreated:*'])
 def s3_handler(event):
   if _is_json(event.key):
+    db = mysql.connector.connect(user=DBUSER, host=DBHOST, password=DBPASS, database=DB)
+    cur = db.cursor()
     # get the file, read it, load it into JSON as an object
     response = s3.get_object(Bucket=S3_BUCKET, Key=event.key)
     text = response["Body"].read().decode()
     data = json.loads(text)
 
     # parse the data fields 1-by-1 from 'data'
-    TITLE = data.get('title') 
-    ALBUM = data.get('album')
-    ARTIST = data.get('artist')
-    YEAR = data.get('year')
-    GENRE = data.get('genre')
+    TITLE = data.get('title', 'Unknown Title')
+    ALBUM = data.get('album', 'Unknown Album')
+    ARTIST = data.get('artist', 'Unknown Artist')
+    YEAR = data.get('year', 0)
+    GENRE = data.get('genre', 'Unknown Genre')
 
     # get the unique ID for the bundle to build the mp3 and jpg urls
     # you get 5 data points in each new JSON file that arrives, but
@@ -66,16 +68,16 @@ def s3_handler(event):
       song_vals = (TITLE, ALBUM, ARTIST, YEAR, MP3, IMG, GENRE)
       cur.execute(add_song, song_vals)
       db.commit()
-      cur.close()
-      db.close()
 
     except mysql.connector.Error as err:
       app.log.error("Failed to insert song: %s", err)
       db.rollback()
+      
+    finally:
+      # Always close cursor and db connection after use
       cur.close()
       db.close()
 
 # perform a suffix match against supported extensions
 def _is_json(key):
   return key.endswith(_SUPPORTED_EXTENSIONS)
-
